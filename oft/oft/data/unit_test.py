d68beaaa7c9f23abec1ad9f1b0e0596df7585dd5
TruongHaSon <85555398+TruongHaSon@users.noreply.github.com>
2023-01-29 12:35:13 +0700

    Update unit_test.py

--- oft/oft/data/unit_test.py
+++ oft/oft/data/unit_test.py
@@ -71,18 +71,15 @@ if __name__ == '__main__':
     idx = 10
     # idx = 34
     kitti_root = "/home/son/Documents/Project/KITTI/object/training"
-    img_file = os.path.join(kitti_root, 'image_2/{:06d}.png'.format(idx))
-    
+    img_file = os.path.join(kitti_root, 'image_2/{:06d}.png'.format(idx))  
     image = np.array(Image.open(img_file))
      
     # Load annotations
     label_file = os.path.join(kitti_root, 'label_2/{:06d}.txt'.format(idx))
-    
     objects = read_kitti_objects(label_file)
 
     # Load calibration matrix
-    calib = os.path.join(kitti_root, 'calib/{:06d}.txt'.format(idx))
-    
+    calib = os.path.join(kitti_root, 'calib/{:06d}.txt'.format(idx))   
     calib = read_kitti_calib(calib)
 
     # Draw 3d bounding boxes
