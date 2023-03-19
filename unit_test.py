b970edf6cb2c5f82345ea4294d305c1773d624e5
TruongHaSon <85555398+TruongHaSon@users.noreply.github.com>
2023-03-19 19:08:28 +0700

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -4,13 +4,13 @@ from data.augmentation import AugmentedObjectDataset
 from visualization.visualization import draw_3d_boxes
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Dataset unit-test
-    kitti_root = "../KITTI"
+    kitti_root = "/content/drive/MyDrive/3d_object_detection/KITTI"
     dataset = KittiObjectDataset(kitti_root)
     idx, image, calib, objects, grid = dataset[0]
-    print("Done")
     # Try some augmentation
     train_image_size = (1080, 360)
     train_grid_size = (120, 120)
@@ -19,7 +19,15 @@ if __name__ == '__main__':
         train_image_size,
         train_grid_size
     )
-    idx, image, calib, objects, grid = aug_dataset[20]
+    idx, image, calib, objects, grid = aug_dataset[1743]
 
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+    #Visualize bounding box
     img = draw_3d_boxes(np.array(image), objects, calib)
     cv2.imwrite("3d_boxes.jpg", img)
+    ax1.imshow(img)
+    ax1.set_title('3D bounding box')
+    #Visualize score
+    vis_score(image, calib, objects, grid, ax=ax2)
+    ax2.set_title('Score')
+    plt.show()
